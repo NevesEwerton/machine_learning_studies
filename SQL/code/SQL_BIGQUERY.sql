16e2baf54fab93c0d7c287f4c072fbdc9e8a71ae
Ewerton <ewerton-25@hotmail.com>
2023-11-28 20:23:53 -0300

    UPDATE sql

---
+++ SQL/code/SQL_BIGQUERY.sql
@@ -0,0 +1,19 @@
+# PROJETANDO OS VALORES DA COLUNA 'NAME'
+
+SELECT 
+  name
+FROM
+  ecommerce.categories;
+
+  # TAMBÉM FUNCIONA PONTERANDO O NOME DO PROJETO
+ SELECT 
+  name
+FROM
+  `sim-project-360116.ecommerce.categories`;
+
+
+ SELECT
+  name, 
+  price
+FROM 
+  `sim-project-360116.ecommerce.products`;
