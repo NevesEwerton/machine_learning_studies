5fcc03443b0405990dc1c43e981c954d49911193
Ewerton <ewerton-25@hotmail.com>
2023-11-29 11:57:51 -0300

    UPDATE SQL_BIGQUERY.sql

--- SQL/code/SQL_BIGQUERY.sql
+++ SQL/code/SQL_BIGQUERY.sql
@@ -17,3 +17,28 @@ FROM
   price
 FROM 
   `sim-project-360116.ecommerce.products`;
+
+
+# PROJETANDO APENAS CINCO LINHAS DA TABELA DESEJADA
+
+SELECT 
+  * 
+FROM ecommerce.categories 
+LIMIT 5
+
+# USO DO DISTINCT - traz os valores não duplicados
+
+SELECT distinct
+  category_id
+from ecommerce.products
+
+SELECT distinct
+  FIRST_NAME
+FROM ecommerce.customers;
+
+
+# NO GOOGLE BIG QUERY NÃO SE COLOCA O APELIDO ENTRE ASPAS
+SELECT 
+  COUNT(distinct FIRST_NAME) AS 'NOMES DISTINTOS'
+FROM
+  ecommerce.customers;
